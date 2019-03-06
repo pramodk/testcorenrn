64ac4d8915bcb826d9fd039d67536c145c82864b
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-03-06 14:30:44 +0100

    Updates for Jenkins tests (#1)
    - Created run script that runs all the tests for 100ms
    - Added `pc.nrnbbcore_write()` to hoc files to output the needed for Coreneuron
    - changed neuron output file from out.dat to out${TEST}.dat
    - Needed to run different tests simultaneously in the same folder
      in jenkins

--- run.sh
+++ run.sh
@@ -2,13 +2,33 @@
 
 names="deriv gf kin conc watch bbcore vecplay patstim"
 
-set -e
+for i in $names ; do
+  mkdir -p test${i}dat
+done
 
 for i in $names ; do
-    mpirun -n 1 special -mpi -c sim_time=100 -c coreneuron=0 test${i}.hoc
-    cat out.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${i}.spk && rm out.dat
-    mpirun -n 1 special -mpi -c sim_time=100 -c coreneuron=1 test${i}.hoc
-    mv out.dat out_cnrn_${i}.spk
-    diff -w -q out_nrn_${i}.spk out_cnrn_${i}.spk
-    rm -f out*.spk
+  mpirun -n 2 ./x86_64/special -mpi -c sim_time=100 test${i}.hoc
+  cat out${i}.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${i}.spk
+  rm out${i}.dat
+done
+
+#prepare coreneuron executable
+
+for i in $names ; do
+  if [ "${i}" = "patstim" ]
+  then
+          mpirun -n 2 ./x86_64/special-core -mpi -d test${i}dat -e 100 --pattern patstim.spk
+  else
+          mpirun -n 2 ./x86_64/special-core -mpi -d test${i}dat -e 100
+  fi
+  cat out.dat > out_cn_${i}.spk
+  rm out.dat
+done
+
+for i in $names ; do
+  DIFF=$(diff -w -q out_nrn_${i}.spk out_cn_${i}.spk)
+  if [ "$DIFF" != "" ] 
+  then
+      echo "Test ${i} failed"
+  fi
 done
