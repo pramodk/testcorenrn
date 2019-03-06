4ec616b451a05284d7bfe7e38812f2fbdcbe3ba0
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-03-06 14:07:35 +0100

    Use 2 processes in mpirun

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ for i in $names ; do
 done
 
 for i in $names ; do
-  mpirun -n 1 ./x86_64/special -mpi -c sim_time=100 test${i}.hoc
+  mpirun -n 2 ./x86_64/special -mpi -c sim_time=100 test${i}.hoc
   cat out${i}.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${i}.spk
   rm out${i}.dat
 done
@@ -17,9 +17,9 @@ done
 for i in $names ; do
   if [ "${i}" = "patstim" ]
   then
-          mpirun -n 1 ./x86_64/special-core -mpi -d test${i}dat -e 100 --pattern patstim.spk
+          mpirun -n 2 ./x86_64/special-core -mpi -d test${i}dat -e 100 --pattern patstim.spk
   else
-          mpirun -n 1 ./x86_64/special-core -mpi -d test${i}dat -e 100
+          mpirun -n 2 ./x86_64/special-core -mpi -d test${i}dat -e 100
   fi
   cat out.dat > out_cn_${i}.spk
   rm out.dat
