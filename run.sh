ccd74a0c723bf3ba45b8ac2c56b3e3d79e3e7c0e
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-03-06 13:49:59 +0100

    use mpirun to run neuron and coreneuron

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ for i in $names ; do
 done
 
 for i in $names ; do
-  ./x86_64/special -c sim_time=100 test${i}.hoc
+  mpirun -n 1 ./x86_64/special -mpi -c sim_time=100 test${i}.hoc
   cat out${i}.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${i}.spk
   rm out${i}.dat
 done
@@ -17,9 +17,9 @@ done
 for i in $names ; do
   if [ "${i}" = "patstim" ]
   then
-          ./x86_64/special-core -d test${i}dat -e 100 --pattern patstim.spk
+          mpirun -n 1 ./x86_64/special-core -mpi -d test${i}dat -e 100 --pattern patstim.spk
   else
-          ./x86_64/special-core -d test${i}dat -e 100
+          mpirun -n 1 ./x86_64/special-core -mpi -d test${i}dat -e 100
   fi
   cat out.dat > out_cn_${i}.spk
   rm out.dat
