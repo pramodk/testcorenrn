f883da280011d6c9bb35ae6783e414df1bcd733a
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2019-07-18 13:17:27 +0200

    Added fixes related to the coreneuron CI and based on neuron master
    - Added dummy real cell to testbbcore to be able to run neuron
      master branch with it
    - Fixed run.sh to run correctly all the tests (the once that have
      only one cell should run on only one process)

--- run.sh
+++ run.sh
@@ -1,34 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-names="deriv gf kin conc watch bbcore vecplay patstim"
+export OMP_NUM_THREADS=1
 
-for i in $names ; do
-  mkdir -p test${i}dat
+declare -A mpi_ranks
+mpi_ranks["bbcore"]=1
+mpi_ranks["conc"]=1
+mpi_ranks["deriv"]=1
+mpi_ranks["gf"]=2
+mpi_ranks["kin"]=1
+mpi_ranks["patstim"]=2
+mpi_ranks["vecplay"]=2
+mpi_ranks["watch"]=2
+
+for test in "${!mpi_ranks[@]}" ; do
+  mkdir -p test${test}dat
 done
 
-for i in $names ; do
-  mpirun -n 2 ./x86_64/special -mpi -c sim_time=100 test${i}.hoc
-  cat out${i}.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${i}.spk
-  rm out${i}.dat
+for test in "${!mpi_ranks[@]}" ; do
+  echo "Running neuron for $test"
+  num_ranks=${mpi_ranks[$test]}
+  srun -n $num_ranks ./x86_64/special -mpi -c sim_time=100 test${test}.hoc
+  cat out${test}.dat | sort -k 1n,1n -k 2n,2n > out_nrn_${test}.spk
+  rm out${test}.dat
 done
 
 #prepare coreneuron executable
-
-for i in $names ; do
-  if [ "${i}" = "patstim" ]
+for test in "${!mpi_ranks[@]}" ; do
+  echo "Running coreneuron for $test"
+  num_ranks=${mpi_ranks[$test]}
+  if [ "${test}" = "patstim" ]
   then
-          mpirun -n 2 ./x86_64/special-core -mpi -d test${i}dat -e 100 --pattern patstim.spk
+    srun -n $num_ranks ./x86_64/special-core -mpi -d test${test}dat -e 100 --pattern patstim.spk
   else
-          mpirun -n 2 ./x86_64/special-core -mpi -d test${i}dat -e 100
+    srun -n $num_ranks ./x86_64/special-core -mpi -d test${test}dat -e 100
   fi
-  cat out.dat > out_cn_${i}.spk
+  cat out.dat > out_cn_${test}.spk
   rm out.dat
 done
 
-for i in $names ; do
-  DIFF=$(diff -w -q out_nrn_${i}.spk out_cn_${i}.spk)
-  if [ "$DIFF" != "" ] 
+for test in "${!mpi_ranks[@]}" ; do
+  DIFF=$(diff -w -q out_nrn_${test}.spk out_cn_${test}.spk)
+  if [ "$DIFF" != "" ]
   then
-      echo "Test ${i} failed"
+    echo "Test ${test} failed"
   fi
 done
